aa4d543f781aea14e53e38f2ff608247d27bcd6e
Raghav Chugh <raaghav1105@gmail.com>
2022-08-27 13:32:06 +0530

    Timers

---
+++ timers/app/src/main/java/com/example/timers/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.timers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
+
+class MainActivity : AppCompatActivity() {
+    private var countDownTimer: CountDownTimer?=null
+    private var timerDuration: Long=60000
+    private var pauseOffset:Long=0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvTimer.text="${(timerDuration/1000).toString()}"
+        btnStart.setOnClickListener {
+            startTimer(pauseOffset)
+        }
+        btnPause.setOnClickListener {
+            pauseTimer()
+        }
+        btnReset.setOnClickListener {
+            resetTimer()
+        }
+    }
+    private fun startTimer(pauseOffsetL: Long){
+        countDownTimer=object : CountDownTimer(timerDuration-pauseOffsetL,1000){
+
+            override fun onTick(millisUntilFinished: Long){
+                pauseOffset= timerDuration-millisUntilFinished
+                tvTimer.text=(millisUntilFinished/1000).toString()
+            }
+            override fun onFinish(){
+                Toast.makeText(this@MainActivity,"Time's up!!",Toast.LENGTH_LONG).show()
+            }
+        }.start()
+
+    }
+
+    private fun pauseTimer(){
+        if(countDownTimer!=null){
+            countDownTimer?.cancel()
+        }
+    }
+    private fun resetTimer(){
+        if(countDownTimer!=null){
+            countDownTimer?.cancel()
+            tvTimer.text="${(timerDuration/1000).toString()}"
+            countDownTimer=null
+            pauseOffset=0
+        }
+    }
+}
